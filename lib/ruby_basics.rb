b3af3eb308c2419b3f356e70e900e42d83c46672
Kevin O'Brien <kpobrien@buffalo.edu>
2019-03-19 20:02:27 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -5,11 +5,11 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-
+  value = "Bob"
 end
 
-def argue
-
+def argue(phrase)
+  phrase = "I'm right and you are wrong!"
 end
 
 def greeting
