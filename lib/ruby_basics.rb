94d4f09cfaf0003903b69d7accb34d964a7839fe
Kevin O'Brien <kpobrien@buffalo.edu>
2019-03-19 20:05:55 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -18,13 +18,13 @@ def greeting(greeting,name)
 end
 
 def return_a_value
-
+  return("Nice")
 end
 
 def last_evaluated_value
-
+  return("expert")
 end
 
 def pizza_party
-
+  return("cheese")
 end
